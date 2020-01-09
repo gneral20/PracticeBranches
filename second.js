a490aeef02981c3408d8652203aff7b138d94c6c
gneral20 <gneral20@gmail.com>
2020-01-09 14:52:48 +0300

    i add second class to second.js

---
+++ second.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Test extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>
+                    hi
+                </h1>
+            </div>
+         );
+    }
+}
+ 
+export default Test;
+
